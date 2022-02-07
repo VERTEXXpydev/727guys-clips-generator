f60fd0382caa674c49373f7c4501ef7bdfc73481
Luke <lukas.zahoransky@zsbystricany.info>
2022-02-07 18:08:54 +0100

    main stuff

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+
+def api_request(endpoint: str, parameters):
+    return requests.get(f'https://osu.ppy.sh/api/{endpoint}', params=parameters).json()
+
+
+def main():
+    #placeholder
+    pass
+
+
+if __name__ == '__main__':
+    main()
