9edf731cda2d227bf09cdb96be0decf185bb2b1c
Michael Ďalog <sesskuseki@gmail.com>
2022-02-08 15:00:20 +0100

    testing commit & base for bot

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
 import json
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix=".")
+bot.remove_command("help")
 
 def api_request(endpoint: str, parameters: dict):
     return requests.get(f'https://osu.ppy.sh/api/{endpoint}', params=parameters).json()
 
+async def log(text):
+    return requests.get('https://canary.discord.com/api/webhooks/940606033750024193/Xem3TJaZ1DlBfaI4aFLV3hDSvG2x4eh9kRxH9xK7rL_ahydzBjPqafq41KoZO1u3u5oS', params={"content": text}).json()
 
-def main():
-    #placeholder
-    pass
+def disp(text):
+    print(text)
+    log(text)
 
+@bot.event
+async def on_ready():
+    disp("Ready.")
 
-if __name__ == '__main__':
-    main()
+bot.run(TOKEN)
