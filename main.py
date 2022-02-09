4c08db8a54d1ad96bdaf5e6a43e28b94c9dc655e
Michael Ďalog <sesskuseki@gmail.com>
2022-02-09 12:12:18 +0100

    replay downloader done

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=".")
+TOKEN = open("token.txt", "r").read()
 bot.remove_command("help")
 
 def api_request(endpoint: str, parameters: dict):
     return requests.get(f'https://osu.ppy.sh/api/{endpoint}', params=parameters).json()
 
-async def log(text):
-    return requests.get('https://canary.discord.com/api/webhooks/940606033750024193/Xem3TJaZ1DlBfaI4aFLV3hDSvG2x4eh9kRxH9xK7rL_ahydzBjPqafq41KoZO1u3u5oS', params={"content": text}).json()
-
-def disp(text):
-    print(text)
-    log(text)
-
 @bot.event
 async def on_ready():
-    disp("Ready.")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Laffey suck dicks"))
+    print("[BOT] Logged into Discord Gateway, starting to look for replays...")
+
+@bot.event
+async def on_message(message):
+    if message.channel.id == 930823120087023696:
+        if str(message.attachments) == "[]": # Checks if there is an attachment on the message
+            return
+        else: # if there is it gets the filename from message.attachments
+            split_v1 = str(message.attachments).split("filename='")[1]
+            filename = str(split_v1).split("' ")[0]
+            if filename.endswith(".osr"): # hecks if it is a .osr file
+                await message.attachments[0].save(fp="replays/{}".format(filename)) # saves the file
+
 
 bot.run(TOKEN)
